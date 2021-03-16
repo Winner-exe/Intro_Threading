0a4139958231d9e3a0d6f90d1472e058c5f77131
Winston Lee <winstonleemail@gmail.com>
2021-03-16 09:14:17 -0400

    Created intro threading project.

---
+++ src/Counter.java
@@ -0,0 +1,22 @@
+public class Counter implements Runnable
+{
+    private int count;
+    private final int id;
+    private static int id_count = 0;
+
+    public Counter()
+    {
+        count = 0;
+        id_count++;
+        id = id_count;
+    }
+
+    public void run()
+    {
+        while (count <= 50)
+        {
+            System.out.println("Counter " + id + ": " + count);
+            count++;
+        }
+    }
+}
---
+++ src/ThreadDriver.java
@@ -0,0 +1,16 @@
+public class ThreadDriver
+{
+    public static void main(String[] args)
+    {
+        Counter[] counters = new Counter[3];
+        for (int i = 0; i < counters.length; i++)
+            counters[i] = new Counter();
+
+        Thread[] threads = new Thread[3];
+        for (int i = 0; i < threads.length; i++)
+        {
+            threads[i] = new Thread(counters[i]);
+            threads[i].start();
+        }
+    }
+}
